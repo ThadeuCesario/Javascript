896238e8bce4c8d00b1b37545c01c176b18028b4
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-03-10 20:56:43 -0300

    Ways to load PHP content

--- cod3r/index.php
+++ cod3r/index.php
@@ -17,13 +17,13 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 - Básico</h3>
+                    <h3>Módulo 01</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">Olá PHP</a>
                         </li>
                         <li>
-                            <a href="#">Exercício B</a>
+                            <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
                         </li>
                         <li>
                             <a href="#">Exercício C</a>
