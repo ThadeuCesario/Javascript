47f7938f8c9ac454c6c01df02982c4c4528e6878
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-05-18 21:17:25 -0300

    Comentários PHP

--- cod3r/basico/comentários.php
+++ cod3r/basico/comentários.php
@@ -9,6 +9,8 @@
    *  linhas
    *  Comentários de múltiplas linhas são mais fortes que o bloco de encerramento
    * '?>', diferente dos comentários de uma única linha.
+   * 
+   * Comentários em PHP são removidos pelo lado do servidor.
    */ 
   
   # Outra forma de comentar.
